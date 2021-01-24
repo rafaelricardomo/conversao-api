c079a8b8b858387e277b511de259c225752726f0
rafaelricardomo <rafaelricardomo@hotmail.com>
2021-01-24 17:23:22 -0300

    [conversao-api] initial commit

---
+++ Conversao.Api/Controllers/ConvertController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Conversao.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConvertController : ControllerBase
+    {
+
+        private readonly ILogger<ConvertController> _logger;
+
+        public ConvertController(ILogger<ConvertController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Fahrenheit/{value}/Celsius")]
+        public decimal GetConvertFahrenheitToCelsius(decimal value)
+        {
+            return TemperatureHelper.ConvertFahrenheitToCelsius(value);
+        }
+
+        [HttpGet]
+        [Route("Celsius/{value}/Fahrenheit")]
+        public decimal ConvertCelsiusToFahrenheit(decimal value)
+        {
+            return TemperatureHelper.ConvertCelsiusToFahrenheit(value);
+        }
+    }
+}
---
+++ Conversao.Api/Helpers/TemperatureHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conversao.Api
+{
+    public static class TemperatureHelper
+    {
+        
+        //Para converter graus Celsius em graus Fahrenheit, multiplique por 1,8 e adicione 32.        
+        public static decimal ConvertCelsiusToFahrenheit(decimal value)
+        {
+            return ((value * 1.8m) + 32);
+        }
+        // Para converter graus Fahrenheit em graus Celsius, subtraia 32 e divida por 1,8.
+        public static decimal ConvertFahrenheitToCelsius(decimal value)
+        {
+            return ((value - 32m) / 1.8m);
+        }
+
+    }
+}
